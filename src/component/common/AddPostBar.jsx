833652f1b1051a244a1371a02c8c4ba1e796c158
unique011 <harshitsharma.2021cs@technonjr.org>
2024-01-25 00:08:33 +0530

    plus

--- src/component/common/AddPostBar.jsx
+++ src/component/common/AddPostBar.jsx
@@ -16,8 +16,8 @@ function AddPostBar() {
         </div>
       </Link>
       <Link to="/add-blog" className="fixed bottom-5 right-5 md:hidden z-20">
-        <div className="rounded-[100%] shadow-[0_10px_20px_rgba(0,_0,_0,_0.7)] bg-primary flex items-center justify-center  hover:-translate-y-1 ">
-          <FaPlus className="h-16 w-16 text-accent" />
+        <div className="rounded-[100%] p-1 shadow-[0_10px_20px_rgba(0,_0,_0,_0.7)] bg-primary flex items-center justify-center  hover:-translate-y-1 ">
+          <FaPlus className="h-14 w-14 text-accent" />
         </div>
       </Link>
     </div>
